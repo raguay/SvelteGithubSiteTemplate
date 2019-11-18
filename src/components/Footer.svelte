b58b4d7a08879112bb78c7309f7c538dddee299b
Richard Guay <raguay@customct.com>
2019-11-18 20:50:50 +0700

    Added partials, dynamic styling, etc.

---
+++ src/components/Footer.svelte
@@ -0,0 +1,43 @@
+<footer style="background-color: {styles.divColor}; background-image: {styles.divBackgroundPicture}; border: {styles.borderSize} solid {styles.borderColor}; border-radius: {styles.borderRadius}; color: {styles.textColor};">
+  <div id='leftBox'>
+    <p>Created using <a href='https://github.com/raguay/SvelteGithubSiteTemplate'>SvelteWeb</a></p>
+    <p>Copyrighted by Richard Guay</p>
+  </div>
+</footer>
+
+<style>
+  footer {
+    width: 85%;
+    margin: auto;
+    padding: 10px;
+  }
+
+  #leftBox {
+    float: left;
+    display: flex;
+    flex-direction: column;
+  }
+
+  #leftBox p {
+    margin: 0px;
+    padding: 0px;
+  }
+
+</style>
+
+<script>
+  import { onMount } from 'svelte';
+  import { info } from '../store/infoStore.js';
+ 
+  let styles = {};
+
+  onMount(() => {
+    //
+    // Subscribe to the information store to get the site information.
+    //
+    info.subscribe((value) => {
+      styles = value.styles;
+    });
+  });
+</script>
+
---
+++ src/components/NavBar.svelte
@@ -0,0 +1,55 @@
+<div id='navbar' style="background-color: {styles.divColor}; background-image: {styles.divbackgroundPicture}; 
+                        border: {styles.borderSize} solid {styles.borderColor}; border-radius: {styles.borderRadius}; 
+                        color: {styles.textColor};" >
+  <a class='navItem' href='/' use:link use:active>Home</a>
+  <a class='navItem' href='/blog/index' use:link use:active>Blog</a>
+  <a class='navItem' href='/about' use:link use:active>About</a>
+</div>
+
+<style>
+  .navItem a {
+    color: black;
+    text-decoration-color: black;
+  }
+
+  #navbar {
+    display: flex;
+    flex-direction: row;
+    width: 85%;
+    margin: auto;
+    border-radius: 10px;
+    padding: 10px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+
+  #navbar a {
+    text-decoration: none;
+    font-size: 24px;
+  }
+
+  .navItem {
+    margin-right: 26px;
+    text-decoration: none;
+  }
+</style>
+
+<script>
+  import Router, { link } from 'svelte-spa-router';
+  import active from 'svelte-spa-router/active';
+  import { onMount } from 'svelte';
+  import { info } from '../store/infoStore.js';
+ 
+  let styles = {};
+
+  onMount(() => {
+    //
+    // Subscribe to the information store to get the site information.
+    //
+    info.subscribe((value) => {
+      styles = value.styles;
+    });
+  });
+
+</script>
+
